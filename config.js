20417698803ed0de26b992e570e51fdb4d9f25da
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-04 16:27:11 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  // Paste your Apps Script Web App /exec URL here after deploying
+  API_URL: 'https://script.google.com/macros/s/PASTE_YOUR_EXEC_URL/exec',
+  APP_NAME: 'Daily Production Review',
+  PAGE_SIZE: 50
+};
