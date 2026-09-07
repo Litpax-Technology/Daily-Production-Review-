530195129d2f6aafb5fc28e27d24a4d667e4a357
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-07 11:05:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Paste your Apps Script Web App /exec URL here after deploying
-  API_URL: 'https://script.google.com/macros/s/AKfycbyMyXRA_kr8UwuAfEziKYnW-oTn0Vnbg2WaMd_-BJRyTr9KHxqjjW4tnRKEUUpXpp9Ivg/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbz72c8VpV7_1oo6I2FjIXRG5O5yd9Pjpo_kWyJfB04aTTz_EWZBz7vi2LSOePZO8hRbqQ/exec',
   APP_NAME: 'Daily Production Review',
   PAGE_SIZE: 50
 };
